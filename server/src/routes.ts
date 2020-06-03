521120e4321d7c114cde27af77a46d2ceff84c2d
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-06-03 18:48:42 -0300

    feat: cadastro de novos pontos de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,63 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello' });
+//listar os itens de coleta
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  });
+
+  return response.json(serializedItems);
+});
+
+//criacao dos pontos de coleta
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map( (item_id: number) => {
+    return {//tuplas da tabela pivot
+      item_id,
+      point_id,
+    };
+  });
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ success: true });
+
 });
 
 export default routes;
