7d77fa4659fcb15d8373c1b1d1323f2ed7b0b763
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-06-03 00:35:45 -0300

    feat: criado todas as migrations e script de execução delas

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('point_items', table => {
+    table.increments('id').primary();
+    
+    table.integer('point_id')
+      .notNullable()
+      .refereces('id')
+      .inTable('points');
+
+    table.integer('item_id')
+      .notNullable()
+      .refereces('id')
+      .inTable('items');
+  })
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('point_items');
+}
