51b92ecfa63ba60cfa8dbc15d314210f56e35b8b
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-06-04 14:35:29 -0300

    feat: construcao de toda estrutura HTML da homepage e formulario

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,127 @@
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
   return (
-    <h1>Pontos</h1>
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+
+      <form>
+        <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        {/* UPLOAD */}
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+
+          <div className="field-group">
+          <div className="field">
+            <label htmlFor="email">E_mail</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="whatsapp">WHatsapp</label>
+            <input
+              type="text"
+              name="whatsapp"
+              id="whatsapp"
+            />
+          </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+
+          {/* MAPA */}
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+            <span>Selecione um ou mais ítens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="lampadas"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/baterias.svg" alt="baterias"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/papeis-papelao.svg  " alt="papeis-papelao"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/eletronicos.svg" alt="eletronicos"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li className="selected">
+              <img src="http://localhost:3333/uploads/organicos.svg" alt="organicos"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+          </ul>
+
+        </fieldset>
+
+        <button type="submit">
+          Cadastrar ponto de coleta
+        </button>
+      </form>
+    </div>
   )
 };
 
