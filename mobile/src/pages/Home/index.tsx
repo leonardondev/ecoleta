24e79d86cd81ee3f29504bd888802b72c1b601f7
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-06-07 13:56:26 -0300

    feat: added filtro de uf, cidade e itens nos pontos do mapa

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,42 +1,71 @@
-import React from 'react';
-import { View, Image, StyleSheet, Text, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, Text, ImageBackground, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+  const [uf,setUf] = useState('');
+  const [city,setCity] = useState('');
+  
   const navigation = useNavigation();
 
   function handleNavigationPoints() {
-    navigation.navigate('Points');
+    navigation.navigate('Points', {
+      uf,
+      city,
+    });
   }
 
   return (
-    <ImageBackground 
-      source={require('../../assets/home-background.png')} 
-      style={styles.container}
-      imageStyle={{ width: 274, height:368 }}
+    <KeyboardAvoidingView 
+      style={{ flex:1 }}
+      behavior={Platform.OS ==='ios' ? 'padding' : undefined}
     >
-      <View style={styles.main}>
-        <Image source={require('../../assets/logo.png')} />
-        <Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
-        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-      </View>
+      <ImageBackground 
+        source={require('../../assets/home-background.png')} 
+        style={styles.container}
+        imageStyle={{ width: 274, height:368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+          <Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
+          <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+        </View>
 
-      <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={handleNavigationPoints}>
-          <View style={styles.buttonIcon}>
-            <Text>
-              <Icon name="arrow-right" color="#FFF" size={24} />
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a UF"
+            value={uf}
+            maxLength={2}
+            autoCapitalize="characters"
+            autoCorrect={false}
+            onChangeText={text => setUf(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a cidade"
+            value={city}
+            autoCorrect={false}
+            onChangeText={text => setCity(text)}
+          />
+
+          <RectButton style={styles.button} onPress={handleNavigationPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right" color="#FFF" size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>
+              Entrar
             </Text>
-          </View>
-          <Text style={styles.buttonText}>
-            Entrar
-          </Text>
-        </RectButton>
-      </View>
+          </RectButton>
+        </View>
 
-    </ImageBackground>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
