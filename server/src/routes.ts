27a6f7a6e71251c2a882b64457302c60f039f8c5
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-06-08 00:42:29 -0300

    feat: add testes de validacao

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -25,7 +26,25 @@ routes.get('/points/:id', pointsController.show);
 
 
 //criacao dos pontos de coleta
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      nome: Joi.string().required(),
+      email: Joi.string().required(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  } 
+  ),
+  pointsController.create);
 
 
 export default routes;
